55fcb763c34de6ffb7dc4c4fd9e2d959d5330a32
Ömer Ali SELEK <bentanerselek@gmail.com>
2020-03-25 12:55:23 +0200

    Blank Page Created

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,15 @@
+<footer class="main-footer">
+    <strong>&copy; <?php echo date("Y"); ?> <a href="https://mrvsoft.com">MRVSOFT</a>  Tüm Hakları Saklıdır.</strong>
+
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0.2
+    </div>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
