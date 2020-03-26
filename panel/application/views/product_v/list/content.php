0d1bb65168f23ed9530470daf8e78e57917b3630
Ömer Ali SELEK <bentanerselek@gmail.com>
2020-03-26 13:58:00 +0200

    Product List Created

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,55 @@
-Product Ürün Listeleme
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Ürün Listesi</h3>
+
+                <div class="card-tools">
+                    <div class="input-group input-group-sm" style="width: 150px;">
+                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Url</th>
+                        <th>Başlık</th>
+                        <th>Açıklama</th>
+                        <th>Durumu</th>
+                        <th>Silme</th>
+                        <th>Düzenleme</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>pc-monitor</td>
+                        <td>Pc Monitör</td>
+                        <td>Açıklama Açıklama Açıklama Açıklama Açıklama Açıklama </td>
+                        <td><div class="form-group">
+                                <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitch3">
+                                    <label class="custom-control-label" for="customSwitch3"></label>
+                                </div></td>
+                        <td><button type="button" class="btn btn-block btn-outline-danger btn-sm"><i class="fas fa-trash-alt"></i>  SİL</button> </td>
+                        <td><button type="button" class="btn btn-block btn-outline-success btn-sm"> <i class="fas fa-edit"></i> DÜZENLE</button></td>
+
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+</div>
+<!-- /.row -->
