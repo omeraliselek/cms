f2d13e67d0f8d7a6a7efe9b13dbcd8d32c7eee92
Ömer Ali SELEK <bentanerselek@gmail.com>
2020-03-26 17:10:04 +0200

    Product list

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -8,14 +8,18 @@ class Product extends CI_Controller {
         parent::__construct();
 
         $this->ViewFolder="product_v";
+
+        $this->load->model("product_model");
     }
 
   public function index()
 
   {
        $viewData = new stdClass();
+      $items = $this->product_model->get_all();
        $viewData->ViewFolder=$this->ViewFolder;
       $viewData->subViewFolder="list";
+      $viewData->items=$items;
 
 
       $this->load->view("{$viewData->ViewFolder}/{$viewData->subViewFolder}/index",$viewData);
--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -2,18 +2,23 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Ürün Listesi</h3>
+                <h3 class="card-title">Ürün Listesi
+
+                </h3>
 
                 <div class="card-tools">
-                    <div class="input-group input-group-sm" style="width: 150px;">
-                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                        </div>
-                    </div>
+                    <button type="button" class="btn btn-block btn-outline-success btn-sm"> <i class="fas fa-plus"></i> Yeni Ekle</button>
                 </div>
             </div>
+
+           <?php if(empty($items)) { ?>
+            <div class="alert alert-info text-center">
+
+
+                Yeni ürün eklemek için <a href="">tıklayın</a>
+            </div>
+
+            <?php }  else { ?>
             <!-- /.card-header -->
             <div class="card-body table-responsive p-0">
                 <table class="table table-hover text-nowrap">
@@ -30,26 +35,36 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>pc-monitor</td>
-                        <td>Pc Monitör</td>
-                        <td>Açıklama Açıklama Açıklama Açıklama Açıklama Açıklama </td>
-                        <td><div class="form-group">
-                                <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
-                                    <input type="checkbox" class="custom-control-input" id="customSwitch3">
-                                    <label class="custom-control-label" for="customSwitch3"></label>
-                                </div></td>
-                        <td><button type="button" class="btn btn-block btn-outline-danger btn-sm"><i class="fas fa-trash-alt"></i>  SİL</button> </td>
-                        <td><button type="button" class="btn btn-block btn-outline-success btn-sm"> <i class="fas fa-edit"></i> DÜZENLE</button></td>
+                    <?php foreach ($items as $item) { ?>
+                        <tr>
+                            <td><?php echo $item->id; ?></td>
+                            <td><?php echo $item->url; ?></td>
+                            <td><?php echo $item->title; ?></td>
+                            <td> <?php echo $item->description; ?></td>
+                            <td> <!-- Bootstrap Switch -->
+                                <input
+                                    type="checkbox"
+                                    data-switchery
+                                    data-color="#10c469"
+                                    <?php echo ($item->isActive) ? "checked" : ""; ?>
+                                />
 
-                    </tr>
+                               </td>
+                            <td><button type="button" class="btn btn-block btn-outline-danger btn-sm"><i class="fas fa-trash-alt"></i>  SİL</button> </td>
+                            <td><button type="button" class="btn btn-block btn-outline-success btn-sm"> <i class="fas fa-edit"></i> DÜZENLE</button></td>
+
+                        </tr>
+                    <?php } ?>
                     </tbody>
                 </table>
+
+                <?php } ?>
             </div>
             <!-- /.card-body -->
         </div>
         <!-- /.card -->
     </div>
 </div>
+
+
 <!-- /.row -->
